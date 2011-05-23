9533bae92b7807c601b5aa6f77be1d942f41f82e
Greg Symons <gsymons@gsconsulting.biz>
2011-05-23 17:40:37 -0500

    Initial layout of the home page

---
+++ _plugins/next_meeting_date.rb
@@ -0,0 +1,44 @@
+require "rubygems"
+require "liquid"
+
+DAYS_IN_WEEK = 7
+SUNDAY = 0
+MONDAY = 1
+
+class Date
+    def nextMonday
+        if self.wday == SUNDAY then
+            self + 1
+        else
+            self + DAYS_IN_WEEK - (self.wday - MONDAY)
+        end
+    end
+end
+
+module Jekyll
+    BASE_DATE = Date.parse('2011-05-23')
+
+    
+    class NextMeetingDate < Liquid::Tag
+        def initialize tag_name, today, tokens
+            super
+            @today = Date.parse(today)
+        end
+
+        def render context
+            nextDate = BASE_DATE
+            if @today > BASE_DATE then
+                if @today.wday != MONDAY then
+                    @today = @today.nextMonday
+                end
+                
+                weeksSinceBaseDate = (@today - BASE_DATE) / DAYS_IN_WEEK
+                offsetToBiweekly = weeksSinceBaseDate % 2
+                weeksFromBaseDateToNextMeeting = weeksSinceBaseDate + offsetToBiweekly
+                nextDate = BASE_DATE + (weeksFromBaseDateToNextMeeting * DAYS_IN_WEEK)
+            end
+                
+            nextDate.strftime('%B %e, %Y')
+        end
+    end
+end
